457e0df6d47358df4efe9ad6614143558b070f4e
aLuizab <aluiza.primo@gmail.com>
2025-04-24 20:39:02 -0300

    aws_lb

---
+++ modules/cell/networking.tf
@@ -0,0 +1,62 @@
+resource "aws_lb" "this" {
+  name               = "${var.environment}-${var.cell_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = aws_subnet.public[*].id
+
+  tags = {
+    Name = "${var.environment}-${var.cell_name}-alb"
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "${var.environment}-${var.cell_name}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.cell_name}-alb-sg"
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = "${var.environment}-${var.cell_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.this.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
